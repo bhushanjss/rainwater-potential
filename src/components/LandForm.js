1a6c23eac0036656498966a954242d6c7dc8ef11
Vidya <bhushanjss@gmail.com>
2020-07-05 16:46:55 +0100

    add district and custom location detection.

--- src/components/LandForm.js
+++ src/components/LandForm.js
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
+import {find, propEq} from 'ramda';
 import { withStyles } from '@material-ui/styles';
 import { Grid, Button, Card, CardContent, InputLabel, MenuItem, FormControl, 
-    Select  } from '@material-ui/core';  
+    Select, Divider  } from '@material-ui/core';  
+import IconButton from '@material-ui/core/IconButton';
+import Tooltip from '@material-ui/core/Tooltip';
+import MyLocationIcon from '@material-ui/icons/MyLocation';
 import TextField from '@material-ui/core/TextField';  
 import api from '../api';  
 import ShowResult from './ShowResult';
+import statesDist from '../resources/state-districts.json';
 
 
 const styles = theme => ({
     widthFull: {
-        width: '100%'
+        width: '100%',
+        margin: '2em 0'
+    },
+    coords: {
+        display: 'flex'
+    },
+    divider: {
+        margin: '2em 0'
     }
   });
 
@@ -19,6 +31,7 @@ class LandForm extends Component {
         super(props);
         this.state = {
             landFormParams : {
+                areaType:'farm',
                 locationName: '',
                 coords:'',
                 length: '',
@@ -29,9 +42,26 @@ class LandForm extends Component {
                 areaUnit: 'acre'
                                
             },
+            stateSelected: '',
+            selectedState: {},
+            district: '',
+            districtList: [],
             result: null,
             resultValid: false
         }          
+    }
+
+    componentDidMount() {   
+    }
+
+    detectMyLocation = () => {
+        navigator.geolocation.getCurrentPosition( pos => {
+            let lat = pos.coords.latitude;
+            let lng = pos.coords.longitude;
+            lat = Math.round(lat*100)/100;
+            lng = Math.round(lng*100)/100;            
+            this.setState({landFormParams: {...this.state.landFormParams, coords:`${lat},${lng}` }});
+        });
     }
 
     handleChange = (event) => {
@@ -39,8 +69,6 @@ class LandForm extends Component {
         const key = event.target.id;
         console.log(key);
         switch(key) {
-            case 'locationName':
-                return this.setState({landFormParams: {...this.state.landFormParams, locationName: event.target.value}});
             case 'coords':
                 return this.setState({landFormParams: {...this.state.landFormParams, coords: event.target.value}});
             case 'length':
@@ -54,8 +82,31 @@ class LandForm extends Component {
         
     }
 
+    handleStateChange = (event) => {
+        const selectedStateDetails = find(propEq('id', event.target.value), statesDist);
+        if(selectedStateDetails.coords) {
+            return this.setState({stateSelected: event.target.value, selectedStateDetails: selectedStateDetails, landFormParams: 
+                {...this.state.landFormParams, coords: selectedStateDetails.coords, districtList: []}});
+        }
+        return this.setState({stateSelected: event.target.value, selectedStateDetails: selectedStateDetails,
+             districtList: selectedStateDetails.districtData});
+    };
+
+    handleDistrictChange = (event) => {
+        const selectedDist = find(propEq('id', event.target.value), this.state.districtList);
+        const coords = selectedDist.coords;
+        this.setState({district: event.target.value,landFormParams: {...this.state.landFormParams, coords: coords}});
+        if(!coords) {
+            console.log(`Coords Not found for District: ${selectedDist.name}`);
+        }        
+    }
+
     handleAreaUnitChange = (event) => (
         this.setState({landFormParams: {...this.state.landFormParams, areaUnit: event.target.value}})
+    );
+
+    handleAreaTypeChange = (event) => (
+        this.setState({landFormParams: {...this.state.landFormParams, areaType: event.target.value}})
     );
 
     handleLenghtUnitChange = (event) => (
@@ -77,36 +128,81 @@ class LandForm extends Component {
         this.setState({resultValid: false})
     );
 
+    renderDistrict = () => {
+        const { classes } = this.props;
+        if(this.state.districtList[0]) {
+            return (
+                <FormControl id="district-name-form" className={classes.widthFull}>
+                    <InputLabel id="district-name-select-label">District</InputLabel>
+                    <Select
+                        labelId="district-name-select-label"
+                        id="district-name"
+                        value={this.state.district}
+                        onChange={this.handleDistrictChange.bind(this)} >
+                        { this.state.districtList.map( val => <MenuItem key={val.id} value={val.id}>{val.name}</MenuItem>) }
+                    </Select>
+                </FormControl>)
+        }
+    }
 
     render() {    
         const { classes } = this.props;         
             if(this.state.resultValid) {
-                return <ShowResult data={this.state.result} backpage={this.backPage.bind(this)} />
+                return <ShowResult data={this.state.result} areaType={this.state.landFormParams.areaType} 
+                stateSelected={this.state.selectedStateDetails} district={this.state.district} backpage={this.backPage.bind(this)} />
             } else {                
             return (<Card>
                 <CardContent>
-                    <form onSubmit={this.handleSubmit} noValidate autoComplete="off">
-                        <Grid container spacing={3}>
-                            <Grid item xs={12}>
+                <Grid container spacing={3}>  
+                    <Grid item xs={12}>
+                    <form onSubmit={this.handleSubmit} noValidate autoComplete="off">                                              
+                            <FormControl id="area-type" className={classes.widthFull}>
+                                <InputLabel id="areaType-select-label">Area Type</InputLabel>
+                                <Select
+                                    labelId="areaType-select-label"
+                                    id="areaType"
+                                    value={this.state.landFormParams.areaType}
+                                    onChange={this.handleAreaTypeChange.bind(this)} >
+                                    <MenuItem value={'farm'}>Farm</MenuItem>
+                                    <MenuItem value={'household'}>House</MenuItem>
+                                    <MenuItem value={'residential'}>Residential Complex</MenuItem>
+                                    <MenuItem value={'industrial'}>Industrial</MenuItem>
+                                </Select>
+                            </FormControl>
+                            <Divider className={classes.divider} />
+                            <Grid className={classes.coords} item xs={12}>
+                                <Grid item xs={9}> 
                                 <TextField className={classes.widthFull} label="Coordinates" id="coords" 
                                     value={this.state.landFormParams.coords} 
                                     onChange={this.handleChange.bind(this)}  />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField className={classes.widthFull} label="Location" id="locationName" 
-                                value={this.state.landFormParams.locationName} 
-                                onChange={this.handleChange.bind(this)}  /> 
-                            </Grid>          
-                            <Grid item xs={12}>
-                                <TextField className={classes.widthFull}  label="Area" id="area" 
-                                    value={this.state.landFormParams.area} onChange={this.handleChange.bind(this)}  
-                                    type="number"
-                                    InputLabelProps={{
-                                        shrink: true,
-                                    }}
-                                    variant="outlined"/>
-                            </Grid>
-                            <Grid item xs={12}>
+                                </Grid>
+                                <Grid item xs={3}>  
+                                <IconButton color="primary" className={classes.backButton} aria-label="back page" 
+                                 onClick={this.detectMyLocation.bind(this)} component="span">
+                                     <Tooltip title="Find My Location"><MyLocationIcon /></Tooltip>
+                                        
+                                </IconButton>    
+                                </Grid>
+                            </Grid>               
+                            <FormControl id="state-name-form" className={classes.widthFull}>
+                                <InputLabel id="state-name-select-label">State</InputLabel>
+                                <Select
+                                    labelId="state-name-select-label"
+                                    id="state-name"
+                                    value={this.state.stateSelected}
+                                    onChange={this.handleStateChange.bind(this)} >
+                                    { statesDist.map( val => <MenuItem key={val.id} value={val.id}>{val.state}</MenuItem>) }
+                                </Select>
+                            </FormControl>
+                            {this.renderDistrict()}
+                            <Divider className={classes.divider} />        
+                            <TextField className={classes.widthFull}  label="Area" id="area" 
+                                value={this.state.landFormParams.area} onChange={this.handleChange.bind(this)}  
+                                type="number"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                variant="outlined"/>
                             <FormControl id="area-unit" className={classes.widthFull}>
                                 <InputLabel id="areaUnit-select-label">Area Unit</InputLabel>
                                 <Select
@@ -120,9 +216,7 @@ class LandForm extends Component {
                                     <MenuItem value={'sqkm'}>Square Kilometers</MenuItem>
                                     <MenuItem value={'sqmi'}>Square Miles</MenuItem>
                                 </Select>
-                            </FormControl>
-                            </Grid>                 
-                            <Grid item xs={12}>
+                            </FormControl>    
                                 <TextField className={classes.widthFull} label="Length" id="length" 
                                     value={this.state.landFormParams.length} onChange={this.handleChange.bind(this)}  
                                     type="number"
@@ -130,8 +224,6 @@ class LandForm extends Component {
                                         shrink: true,
                                     }}
                                     variant="outlined"/>
-                            </Grid> 
-                            <Grid item xs={12}>
                             <FormControl className={classes.widthFull}>
                                 <InputLabel id="lengthUnit-select-label">Length Unit</InputLabel>
                                 <Select
@@ -145,8 +237,6 @@ class LandForm extends Component {
                                     <MenuItem value={'mile'}>Mile</MenuItem>
                                 </Select>
                             </FormControl>
-                            </Grid> 
-                            <Grid item xs={12}>
                                 <TextField className={classes.widthFull} label="Width" id="width" 
                                     value={this.state.landFormParams.width} onChange={this.handleChange.bind(this)}  
                                     type="number"
@@ -154,8 +244,6 @@ class LandForm extends Component {
                                         shrink: true,
                                     }}
                                     variant="outlined"/>
-                            </Grid> 
-                            <Grid item xs={12}>
                             <FormControl className={classes.widthFull}>
                                 <InputLabel id="lengthUnit-select-label">Width Unit</InputLabel>
                                 <Select
@@ -168,11 +256,12 @@ class LandForm extends Component {
                                     <MenuItem value={'km'}>Kilo Meter</MenuItem>
                                     <MenuItem value={'mile'}>Mile</MenuItem>
                                 </Select>
-                                </FormControl>
-                            </Grid>                            
-                            <Grid item xs={12}><Button variant="contained" type="Submit" color="primary">Submit</Button></Grid>                    
-                        </Grid>
+                            </FormControl>
+                            <Divider className={classes.divider} />
+                            <Button variant="contained" type="Submit" color="primary">Submit</Button>
                     </form>
+                    </Grid>
+                    </Grid>
                 </CardContent>            
             </Card>);
             }                               
--- src/components/ShowResult.js
+++ src/components/ShowResult.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+import CardMedia from '@material-ui/core/CardMedia';
 import IconButton from '@material-ui/core/IconButton';
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
+import CloseIcon from '@material-ui/icons/Close';
 import WavesIcon from '@material-ui/icons/Waves';
 import LandscapeIcon from '@material-ui/icons/Landscape';
 import PinDropIcon from '@material-ui/icons/PinDrop';
+import bucketImgSrc from '../resources/water-tank.png';
 
 const useStyles = makeStyles(theme => ({
     backButton: {
@@ -15,14 +18,56 @@ const useStyles = makeStyles(theme => ({
         margin: '2em 0'
     },
     typography: {
-        padding: '1em 0'
+        padding: '1em 0',
+        display: 'flex',
+        justifyContent: 'space-between'
     },
+    typographyIcon: {
+        marginLeft: '2em'
+    },
+    media: {
+        width: '4em',
+        backgroundSize: 'contain',
+        height: '4em'
+      },
+    icon: {
+        fontSize: '4em',
+    },
+    flexView: {
+        display: 'flex',
+        justifyContent: 'space-around'
+    },
+    volWaterTxt: {
+        fontSize: '2em'
+    }  
   }));
+
+  
+
+  const renderCoordinates = (props, classes) => {      
+    const {data, stateSelected, district} = props;
+    let location = '';
+    console.log(JSON.stringify(stateSelected));
+    if(district) {
+        location = district + ', ' + stateSelected.state;
+    } else if(stateSelected) {        
+        location = stateSelected.state;
+    } else {
+        location = data.coord;
+    }
+    return (
+        <Typography className={classes.typography} color="textSecondary">
+            <PinDropIcon fontSize="large" className={classes.typographyIcon} />
+            <span className="text">{location}</span>                        
+        </Typography>
+    )
+  }
 
 
 export default function ShowResult(props) {
-    const {area, areaUnit, volWaterRounded, waterUnit, coord} = props.data;
     const classes = useStyles();
+    const {area, areaUnit, volWaterRounded, waterUnit} = props.data;
+   
 
     return (
         <div>
@@ -35,18 +80,31 @@ export default function ShowResult(props) {
                     <Typography className={classes.typography} component="h5" variant="h5">
                         Rain Water Received (Yearly)
                     </Typography>
-                    <Typography className={classes.typography} color="textSecondary">
-                        <Grid item sm={12}><PinDropIcon fontSize="large" /></Grid>
-                        <Grid item sm={12}><span className="text">{coord}</span></Grid>                        
-                    </Typography>
-                    <Typography className={classes.typography} color="textSecondary">
-                        <Grid item sm={12}><WavesIcon fontSize="large" /></Grid>
-                        <Grid item sm={12}><span className="text"> {volWaterRounded} {waterUnit}</span></Grid>
-                    </Typography>
-                    <Typography className={classes.typography} color="textSecondary">
-                    <Grid item sm={12}><LandscapeIcon fontSize="large"/></Grid>
-                    <Grid item sm={12}><span className="text"> {area} {areaUnit}</span></Grid>
+                    <Typography className={classes.typography}>
+                        {props.areaType}
                     </Typography>                    
+                    {renderCoordinates(props, classes)}
+                    <Typography className={classes.typography} color="textSecondary">
+                        <LandscapeIcon fontSize="large" className={classes.typographyIcon} />
+                        <span className="text"> {area} {areaUnit}</span>
+                    </Typography>
+                    <Typography className={classes.typography} color="textSecondary">
+                        <WavesIcon fontSize="large" className={classes.typographyIcon} />
+                        <span className="text"> {volWaterRounded} {waterUnit}</span>
+                    </Typography>
+                    
+                    <Grid item sm={12} className={classes.flexView}>
+                        <CardMedia
+                            className={classes.media}
+                            image={bucketImgSrc}
+                            title="Bucket"
+                        /> 
+                        <CloseIcon className={classes.icon} />
+                        <Typography className={classes.volWaterTxt}  variant="subtitle1" color="textSecondary">
+                            { Math.round(volWaterRounded/1000) } 
+                        </Typography>                           
+                    </Grid>
+                                                       
                 </CardContent>
             </Card>
         </div>
